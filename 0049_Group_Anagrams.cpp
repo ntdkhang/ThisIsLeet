85a473ef0083ad4c101b5a59b09f5982597fb751
ntdkhang <ntdkhang143@gmail.com>
2024-05-07 08:41:43 -0500

    back to the grind

--- 0049_Group_Anagrams.cpp
+++ 0049_Group_Anagrams.cpp
@@ -1,11 +1,36 @@
-#include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
-  vector<vector<string>> groupAnagrams(vector<string> &strs) {}
+    vector<vector<string>> groupAnagrams(vector<string> &strs) {
+        unordered_map<string, vector<string>> map;
+        vector<vector<string>> result;
+        for (string word : strs) {
+            string sorted = reorder(word);
+            auto it = map.find(sorted);
+            if (it == map.end()) {
+                map[sorted] = vector<string>{word};
+            } else {
+                it->second.push_back(word);
+            }
+        }
+
+
+        for (auto it = map.begin(); it != map.end(); it++) {
+            result.push_back(it->second);
+        }
+
+        return result;
+    }
+
+    string reorder(const string& word) {
+        string temp = word;
+        sort(temp.begin(), temp.end());
+        return temp;
+    }
 };
 
 int main() {}
