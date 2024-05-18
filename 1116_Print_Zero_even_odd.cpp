9f11e94bcccbb33e3c5929ee1e1423938fe9f680
ntdkhang <ntdkhang143@gmail.com>
2024-05-18 10:12:30 -0500

    1116 sem

--- 1116_Print_Zero_even_odd.cpp
+++ 1116_Print_Zero_even_odd.cpp
@@ -2,9 +2,53 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
+#include <semaphore>
 
 
 
+class ZeroEvenOdd {
+private:
+    int n;
+    std::binary_semaphore sem_z{1}, sem_e{0}, sem_o{0};
+
+
+public:
+    ZeroEvenOdd(int n) {
+        this->n = n;
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void zero(std::function<void(int)> printNumber) {
+        for (int i = 1; i <= n; i++) {
+            sem_z.acquire();
+            printNumber(0);
+            if (i % 2 == 1)
+                sem_o.release();
+            else
+                sem_e.release();
+        }
+    }
+
+    void even(std::function<void(int)> printNumber) {
+        for (int i = 2; i <= n; i += 2) {
+            sem_e.acquire();
+            printNumber(i);
+            sem_z.release();
+        }
+
+    }
+
+    void odd(std::function<void(int)> printNumber) {
+        for (int i = 1; i <= n; i += 2) {
+            sem_o.acquire();
+            printNumber(i);
+            sem_z.release();
+        }
+
+    }
+};
+
+/*
 class ZeroEvenOdd {
 private:
     int n;
@@ -68,3 +112,4 @@ public:
         }
     }
 };
+*/
