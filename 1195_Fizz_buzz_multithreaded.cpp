27835e405171eb07f2b2ccdcf2af4ae60f69c8cd
ntdkhang <ntdkhang143@gmail.com>
2024-05-18 10:53:53 -0500

    1195

--- 1195_Fizz_buzz_multithreaded.cpp
+++ 1195_Fizz_buzz_multithreaded.cpp
@@ -1,90 +1,71 @@
 #include <condition_variable>
 #include <mutex>
-#include <queue>
 
 class FizzBuzz {
 private:
     int n;
-    std::queue<int> q;
+    int i;
     std::mutex mut;
     std::condition_variable cond;
-    // -1 fizz
-    // -2 buzz
-    // -3 fizzbuzz
-    // n  number
 
 public:
     FizzBuzz(int n) {
         this->n = n;
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 != 0) {
-                q.push(-1);
-            } else if (i % 5 == 0 && i % 3 != 0) {
-                q.push(-2);
-            } else if (i % 5 == 0 && i % 3 == 0) {
-                q.push(-3);
-            } else {
-                q.push(i);
-            }
-        }
+        i = 1;
     }
 
     // printFizz() outputs "fizz".
-    // i % 3 == 0 || i % 5 != 0
     void fizz(std::function<void()> printFizz) {
-        while (!q.empty()) {
+        while (i <= n) {
             std::unique_lock<std::mutex> lock(mut);
-            cond.wait(lock, [this] { return q.front() == -1 || q.empty(); });
+            cond.wait(lock, [this] { return (i % 3 == 0 && i % 5 != 0) || i > n; });
 
-            if (q.empty())
+            if (i > n)
                 return;
             printFizz();
-            q.pop();
+            i++;
             cond.notify_all();
         }
     }
 
     // printBuzz() outputs "buzz".
-    // i % 5 == 0 || i % 3 != 0
     void buzz(std::function<void()> printBuzz) {
-        while (!q.empty()) {
+        while (i <= n) {
             std::unique_lock<std::mutex> lock(mut);
-            cond.wait(lock, [this] { return q.front() == -2 || q.empty(); });
+            cond.wait(lock, [this] { return (i % 5 == 0 && i % 3 != 0) || i > n; });
 
-            if (q.empty())
+            if (i > n)
                 return;
             printBuzz();
-            q.pop();
+            i++;
             cond.notify_all();
         }
     }
 
     // printFizzBuzz() outputs "fizzbuzz".
-    // i % 5 == 0 || i % 3 == 0
 	void fizzbuzz(std::function<void()> printFizzBuzz) {
-        while (!q.empty()) {
+        while (i <= n) {
             std::unique_lock<std::mutex> lock(mut);
-            cond.wait(lock, [this] { return q.front() == -3 || q.empty(); });
+            cond.wait(lock, [this] { return (i % 5 == 0 && i % 3 == 0) || i > n; });
 
-            if (q.empty())
+            if (i > n)
                 return;
             printFizzBuzz();
-            q.pop();
+            i++;
             cond.notify_all();
         }
     }
 
     // printNumber(x) outputs "x", where x is an integer.
-    // i % 5 != 0 || i % 3 != 0
     void number(std::function<void(int)> printNumber) {
-        while (!q.empty()) {
+        while (i <= n) {
             std::unique_lock<std::mutex> lock(mut);
-            cond.wait(lock, [this] { return q.front() > 0 || q.empty(); });
+            cond.wait(lock, [this] { return (i % 5 != 0 && i % 3 != 0) || i > n; });
 
-            if (q.empty())
+            if (i > n)
                 return;
-            printNumber(q.front());
-            q.pop();
+            printNumber(i);
+            i++;
             cond.notify_all();
         }
     }
