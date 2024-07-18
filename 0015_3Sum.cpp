1e6e0779693c2fe47bdfab9b2667142bbbb7fad2
ntdkhang <ntdkhang143@gmail.com>
2024-07-18 15:01:55 -0500

    15 fast

--- 0015_3Sum.cpp
+++ 0015_3Sum.cpp
@@ -1,39 +1,34 @@
+#include <iostream>
 #include <vector>
-#include <map>
 
 class Solution {
 public:
     std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
         std::vector<std::vector<int>> result;
-        std::map<int, int> count;
-        std::vector<int> keys;
+        sort(nums.begin(), nums.end());
 
-        for (std::vector<int>::iterator it = nums.begin(); it != nums.end(); it++) {
-            count[*it]++;
-        }
-
-        for (std::map<int, int>::iterator imap = count.begin(); imap != count.end(); imap++) {
-            keys.push_back(imap->first);
-        }
-
-        for (int i = 0; i < keys.size(); i++) {
-            // decrement count of the first number;
-            int firstNum = keys[i];
-            count[firstNum]--;
-            for (int j = i; j < keys.size(); j++) {
-                int secondNum = keys[j];
-                if (count[secondNum] > 0) {
-                    count[secondNum]--;
-                    int thirdNum = -(firstNum + secondNum);
-                    if (thirdNum >= secondNum && count.find(thirdNum) != count.end() && count[thirdNum] > 0) {
-                        result.push_back(std::vector<int>{firstNum, secondNum, thirdNum});
-                    }
-                    count[secondNum]++;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int target = -nums[i];
+            int left = i+1, right = nums.size() - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    result.push_back(std::vector<int>{nums[i], nums[left], nums[right]});
+                    do {
+                        left++;
+                    } while (left < right && nums[left] == nums[left-1]);
+                    do {
+                        right--;
+                    } while (right > left && nums[right] == nums[right+1]);
+                } else if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
                 }
             }
-
-            // increment count of first
-            count[firstNum]++;
         }
 
         return result;
